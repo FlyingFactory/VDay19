9336cf0dc28d1f8cda8d9b8d139ab0c5b85e3ec4
FlyingFactory <36451516+FlyingFactory@users.noreply.github.com>
2019-02-12 17:48:20 +0800

    Star map fully working(?)

---
+++ Assets/Scripts/Merrick/DetectPlacementStarMap.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPlacementStarMap : MonoBehaviour {
+
+    private bool detect1 = false;
+    [SerializeField] private GameObject trigger1 = null;
+    private bool detect2 = false;
+    [SerializeField] private GameObject trigger2 = null;
+
+    [SerializeField] private GameObject successDisplay = null;
+    private float currentDisplayTime = 0;
+    [SerializeField] private float displayTime = 5f;
+    private bool startedDisplay = false;
+    private bool endedDisplay = false;
+
+
+    private float currentDetectionTime = 0;
+    [SerializeField] private float detectionTime = 2f;
+    
+    [SerializeField] private float bufferMult = 1.5f;
+
+    private void Start() {
+        if (trigger1 == null || trigger2 == null || successDisplay == null) {
+            Debug.LogWarning("Missing reference in DetectPlacement, deleting");
+            Destroy(this);
+        }
+    }
+
+    private void Update() {
+        if (!startedDisplay) {
+            if (detect1 && detect2) {
+                currentDetectionTime += Time.deltaTime;
+                if (currentDetectionTime > detectionTime) {
+                    startedDisplay = true;
+                    successDisplay.SetActive(true);
+                }
+            }
+            else {
+                currentDetectionTime = Mathf.Max(0, currentDetectionTime - Time.deltaTime * bufferMult);
+            }
+        }
+        else if (!endedDisplay) {
+            currentDisplayTime += Time.deltaTime;
+            if (currentDisplayTime > displayTime) {
+                endedDisplay = true;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.gameObject.GetInstanceID() == trigger1.GetInstanceID()) {
+            detect1 = true;
+        }
+        else if (other.gameObject.GetInstanceID() == trigger2.GetInstanceID()) {
+            detect2 = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) {
+        if (other.gameObject.GetInstanceID() == trigger1.GetInstanceID()) {
+            detect1 = false;
+        }
+        else if (other.gameObject.GetInstanceID() == trigger2.GetInstanceID()) {
+            detect2 = false;
+        }
+    }
+}
--- Assets/Scripts/Merrick/StarMapGenerator.cs
+++ Assets/Scripts/Merrick/StarMapGenerator.cs
@@ -14,15 +14,19 @@ public class StarMapGenerator : MonoBehaviour {
     [SerializeField] private float spaceWidth = 5;
     [SerializeField] private float spaceHeight = 5;
 
-    [SerializeField] [Min(4)] private int numVertices = 10;
-    [SerializeField] [Min(3)] private int pathVertexCount = 5;
+    [SerializeField] private int numVertices = 10;
+    [SerializeField] private int pathVertexCount = 5;
     [SerializeField] [Range(0,100)] private float dummyEdgeChance = 50f;
 
+    
     public void OnValidate() {
+        if (numVertices < 4) numVertices = 4;
+        if (pathVertexCount < 3) pathVertexCount = 3;
         if (pathVertexCount > numVertices) pathVertexCount = numVertices;
     }
 
-    public void Reset() {
+    [ContextMenu("Generate")]
+    public void Generate() {
         if (vertexPrefab != null &&
             edgePrefab != null &&
             vertexMarkerPrefab != null &&
@@ -39,81 +43,93 @@ public class StarMapGenerator : MonoBehaviour {
                                          transform.position + new Vector3(Random.Range(-spaceWidth / 2, spaceWidth / 2), 
                                                                           Random.Range(0, spaceHeight), 
                                                                           Random.Range(-spaceWidth / 2, spaceWidth / 2)), 
-                                         Quaternion.identity, 
-                                         transform));
+                                         Quaternion.identity));
+                vertices[i].transform.parent = transform;
             }
 
             Vector3 pos1;
             Vector3 pos2;
             Transform newEdge;
 
+            Instantiate(vertexMarkerPrefab,
+                        vertices[0].transform.position,
+                        Quaternion.identity)
+                        .transform.parent = vertexCardTransform;
+
             for (int i = 1; i < pathVertexCount; i++) {
                 Instantiate(vertexMarkerPrefab,
                             vertices[i].transform.position,
-                            Quaternion.identity,
-                            transform);
+                            Quaternion.identity)
+                            .transform.parent = vertexCardTransform;
                 pos1 = vertices[i].transform.position;
                 pos2 = vertices[i - 1].transform.position;
-                newEdge = Instantiate(edgePrefab,
-                                    (pos1 + pos2) / 2,
-                                    Quaternion.identity,
-                                    edgeCardTransform).transform;
-                newEdge.localScale = new Vector3(newEdge.localScale.x, (pos1 - pos2).magnitude, newEdge.localScale.z);
-                newEdge.Rotate(new Vector3(pos1.z - pos2.z, 0, pos2.x - pos1.x),
-                               Mathf.Rad2Deg * Mathf.Atan2(pos1.z - pos2.z, Mathf.Sqrt(Mathf.Pow(pos1.x - pos2.x,2) + Mathf.Pow(pos1.z - pos2.z, 2))));
+                MakeEdge(pos1, pos2);
+                //newEdge = Instantiate(edgePrefab,
+                //                      (pos1 + pos2) / 2,
+                //                      Quaternion.identity).transform;
+                //newEdge.parent = edgeCardTransform;
+                //newEdge.localScale = new Vector3(newEdge.localScale.x, newEdge.localScale.y * (pos1 - pos2).magnitude, newEdge.localScale.z);
+                //newEdge.Rotate(new Vector3(pos1.z - pos2.z, 0, pos2.x - pos1.x),
+                //               Mathf.Rad2Deg * Mathf.Atan2(Mathf.Sqrt(Mathf.Pow(pos1.x - pos2.x, 2) + Mathf.Pow(pos1.z - pos2.z, 2)), pos1.y - pos2.y));
             }
 
             for (int i = 0; i < pathVertexCount; i++) {
-                for (int j = pathVertexCount; i < numVertices; j++) {
+                for (int j = pathVertexCount; j < numVertices; j++) {
                     if (Random.Range(0, 100f) < dummyEdgeChance) {
                         pos1 = vertices[i].transform.position;
                         pos2 = vertices[j].transform.position;
-
-                        newEdge = Instantiate(edgePrefab,
-                                              (pos1 + pos2) / 2,
-                                              Quaternion.identity,
-                                              edgeCardTransform).transform;
-                        newEdge.localScale = new Vector3(newEdge.localScale.x, (pos1 - pos2).magnitude, newEdge.localScale.z);
-                        newEdge.Rotate(new Vector3(pos1.z - pos2.z, 0, pos2.x - pos1.x),
-                                       Mathf.Rad2Deg * Mathf.Atan2(pos1.z - pos2.z, Mathf.Sqrt(Mathf.Pow(pos1.x - pos2.x, 2) + Mathf.Pow(pos1.z - pos2.z, 2))));
-
-                        newEdge = Instantiate(edgeTrapPrefab,
-                                              (pos1 + pos2) / 2,
-                                              Quaternion.identity,
-                                              edgeCardTransform).transform;
-                        newEdge.localScale = new Vector3(newEdge.localScale.x, (pos1 - pos2).magnitude, newEdge.localScale.z);
-                        newEdge.Rotate(new Vector3(pos1.z - pos2.z, 0, pos2.x - pos1.x),
-                                       Mathf.Rad2Deg * Mathf.Atan2(pos1.z - pos2.z, Mathf.Sqrt(Mathf.Pow(pos1.x - pos2.x, 2) + Mathf.Pow(pos1.z - pos2.z, 2))));
+                        MakeEdge(pos1, pos2, true);
                     }
                 }
             }
 
             for (int i = pathVertexCount; i < numVertices-1; i++) {
-                for (int j = i+1; i < numVertices; j++) {
+                for (int j = i+1; j < numVertices; j++) {
                     if (Random.Range(0, 100f) < dummyEdgeChance) {
                         pos1 = vertices[i].transform.position;
                         pos2 = vertices[j].transform.position;
-
-                        newEdge = Instantiate(edgePrefab,
-                                              (pos1 + pos2) / 2,
-                                              Quaternion.identity,
-                                              edgeCardTransform).transform;
-                        newEdge.localScale = new Vector3(newEdge.localScale.x, (pos1 - pos2).magnitude, newEdge.localScale.z);
-                        newEdge.Rotate(new Vector3(pos1.z - pos2.z, 0, pos2.x - pos1.x),
-                                       Mathf.Rad2Deg * Mathf.Atan2(pos1.z - pos2.z, Mathf.Sqrt(Mathf.Pow(pos1.x - pos2.x, 2) + Mathf.Pow(pos1.z - pos2.z, 2))));
-
-                        newEdge = Instantiate(edgeTrapPrefab,
-                                              (pos1 + pos2) / 2,
-                                              Quaternion.identity,
-                                              edgeCardTransform).transform;
-                        newEdge.localScale = new Vector3(newEdge.localScale.x, (pos1 - pos2).magnitude, newEdge.localScale.z);
-                        newEdge.Rotate(new Vector3(pos1.z - pos2.z, 0, pos2.x - pos1.x),
-                                       Mathf.Rad2Deg * Mathf.Atan2(pos1.z - pos2.z, Mathf.Sqrt(Mathf.Pow(pos1.x - pos2.x, 2) + Mathf.Pow(pos1.z - pos2.z, 2))));
+                        MakeEdge(pos1, pos2, true);
                     }
                 }
             }
+        }
+    }
 
+    public void MakeEdge(Vector3 pos1, Vector3 pos2, bool trap = false) {
+        Transform newEdge = Instantiate(edgePrefab,
+                                        (pos1 + pos2) / 2,
+                                        Quaternion.identity).transform;
+        newEdge.parent = transform;
+        newEdge.localScale = new Vector3(newEdge.localScale.x, newEdge.localScale.y * (pos1 - pos2).magnitude, newEdge.localScale.z);
+        newEdge.Rotate(new Vector3(pos1.z - pos2.z, 0, pos2.x - pos1.x),
+                       Mathf.Rad2Deg * Mathf.Atan2(Mathf.Sqrt(Mathf.Pow(pos1.x - pos2.x, 2) + Mathf.Pow(pos1.z - pos2.z, 2)), pos1.y - pos2.y));
 
+        if (trap) {
+            newEdge = Instantiate(edgeTrapPrefab,
+                                  (pos1 + pos2) / 2,
+                                  Quaternion.identity).transform;
+            newEdge.parent = edgeCardTransform;
+            newEdge.localScale = new Vector3(newEdge.localScale.x, newEdge.localScale.y * (pos1 - pos2).magnitude, newEdge.localScale.z);
+            newEdge.Rotate(new Vector3(pos1.z - pos2.z, 0, pos2.x - pos1.x),
+                           Mathf.Rad2Deg * Mathf.Atan2(Mathf.Sqrt(Mathf.Pow(pos1.x - pos2.x, 2) + Mathf.Pow(pos1.z - pos2.z, 2)), pos1.y - pos2.y));
+        }
+    }
+
+    [ContextMenu("Delete all children")]
+    public void DeleteChildren() {
+        for (int i = transform.childCount - 1; i >= 0; i--) {
+            DestroyImmediate(transform.GetChild(i).gameObject);
+        }
+    }
+
+    [SerializeField] private Transform position1;
+    [SerializeField] private Transform position2;
+    [SerializeField] private bool FakeEdge;
+
+    [ContextMenu("Generate single edge")]
+    public void SingleEdge() {
+        if (position1 != null && position2 != null) {
+            MakeEdge(position1.position, position2.position, FakeEdge);
         }
     }
 }
